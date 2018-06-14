f502cb538c4fd4aa6a3fd30014a8ef6be5daf631
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-06-14 16:15:08 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,5 @@
+// a parameter is WHAT KIND of thing that is going to be passed into the function
+//an argument is the ACTUAL THING being passed into the function
+
+//return will actually produce a value and spit it out of the function allowing other things within the program to use that value
+//console.log just shows the developer what value is coming out but doesn't give that info to the program
